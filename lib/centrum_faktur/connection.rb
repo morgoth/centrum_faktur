d46b907617fa558b3368dcf9ed8d32e67913d976
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2012-08-01 13:33:26 +0200

    enabled ssh verification

--- lib/centrum_faktur/connection.rb
+++ lib/centrum_faktur/connection.rb
@@ -15,7 +15,6 @@ module CentrumFaktur
     def http
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       http
     end
 
